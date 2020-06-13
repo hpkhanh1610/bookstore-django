390d4ef20a9aae5a9fcc5f5c80144340c80a9ee2
Huỳnh Phương Khanh <phuongkhanh@tgm.vn>
2020-06-13 17:40:05 +0700

    Added 'setUp' method to avoid repeating variable 'response'

--- pages/tests.py
+++ pages/tests.py
@@ -4,22 +4,18 @@ from django.urls import reverse
 
 class HomepageTests(SimpleTestCase):
 
-    def test_homepage_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def setUp(self): # must use exact name 'setUp', otherwise errors will be thrown
+        url = reverse('home')
+        self.response = self.client.get(url)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(self.response, 'home.html')
 
     def test_homepage_content_html(self):
-        response = self.client.get('/')
-        self.assertContains(response, 'Homepage')
+        self.assertContains(self.response, 'Homepage')
 
     def test_homepage_doesnot_contain_html(self):
-        response = self.client.get('/')
-        self.assertNotContains(response, 'Hey, how are you doing?')
+        self.assertNotContains(self.response, 'Hey, how are you doing?')
