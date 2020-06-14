1cd329c3c65943351c080d41561c4be50a2293e0
Huỳnh Phương Khanh <hpkhanh1610@gmail.com>
2020-06-14 09:51:55 +0700

    Played around with 'reverse_lazy' and 'reverse'

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 
 from .forms import CustomUserCreationForm
@@ -7,6 +7,11 @@ from .forms import CustomUserCreationForm
 class SignupPageView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    # success_url = reverse('login') # will throw an error since URLConf is not loaded
+    # can't use reverse with success_url, 
+    # because then reverse is called when the module is imported, 
+    # before the urls have been loaded.
+    # https://docs.djangoproject.com/en/3.0/ref/class-based-views/mixins-editing/#django.views.generic.edit.FormMixin
+    template_name = 'signup.html'    
 
 
